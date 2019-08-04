ce73eea5056b06e30af19fb1dbae3ea33a27d78a
Enrico Giurin <enricogiurin@gmail.com>
2019-08-04 14:01:24 +0200

    basic definition layout

--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,21 @@
-function turnOnOff(id) {
-    
+function turnOnOff(id) {    
 	let image = document.getElementById(id);
-
 	if (image.src.match("ONbulb")) {
         image.src = "images/OFFbulb.jpg"
     }		
 	else{
         image.src = "images/ONbulb.jpg"; 
-    }
-		
+    }		
+} 
+
+function increaseValue(id) {    
+	let field = document.getElementById(id);
+	var newVal = parseFloat(field.innerText) + 1;
+	field.innerText=newVal;	
+} 
+
+function decreaseValue(id) {    
+	let field = document.getElementById(id);
+	var newVal = parseFloat(field.innerText) - 1;
+	field.innerText=newVal;	
 } 
